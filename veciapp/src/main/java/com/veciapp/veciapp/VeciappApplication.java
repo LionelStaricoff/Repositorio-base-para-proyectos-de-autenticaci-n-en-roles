0ebdb692cd67d21ee91de550fb373cef3b1c2021
lionel staricoff <staricofflionel@hotmail.com>
2025-07-12 18:27:00 -0500

    feat: primer commit

---
+++ veciapp/src/main/java/com/veciapp/veciapp/VeciappApplication.java
@@ -0,0 +1,13 @@
+package com.veciapp.veciapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeciappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeciappApplication.class, args);
+	}
+
+}
