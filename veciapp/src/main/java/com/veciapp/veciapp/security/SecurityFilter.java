92524e6ea2364cb01073dbfefdab2004e6725ccc
lionel staricoff <staricofflionel@hotmail.com>
2025-07-13 22:00:51 -0500

    feat: (SecurityFilter) imprementando la logica para el doFilterInternal()

--- veciapp/src/main/java/com/veciapp/veciapp/security/SecurityFilter.java
+++ veciapp/src/main/java/com/veciapp/veciapp/security/SecurityFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,5 +23,17 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        var authHeader = request.getHeader("AUTORIZATION");
+        if(authHeader != null){
+            var token = authHeader.replace("Bearer ","");
+            var nombreUsuario = tokenService.getSubjetct(token);
+
+            if(nombreUsuario != null){
+                UserDetails usuario = userRepository.findByEmail(nombreUsuario);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
