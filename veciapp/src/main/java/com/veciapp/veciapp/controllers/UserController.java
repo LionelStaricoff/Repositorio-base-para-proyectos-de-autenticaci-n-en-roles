fc19fc3610b81535ae34264ab475a1640ed41a35
lionel staricoff <staricofflionel@hotmail.com>
2025-07-27 16:17:00 -0500

    doc: (test)

--- veciapp/src/main/java/com/veciapp/veciapp/controllers/UserController.java
+++ veciapp/src/main/java/com/veciapp/veciapp/controllers/UserController.java
@@ -25,4 +25,14 @@ public class UserController {
     public ResponseEntity<UserResponseDto> createUSer(@RequestBody @Valid LoginDto loginDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(new UserResponseDto(userService.createUser(loginDto)));
     }
+
+    /**
+     *
+     * mvn clean package
+     *  docker build -t lionelstaricoff/veciapp:v1 .
+     * docker push lionelstaricoff/veciapp:v1
+     * https://dashboard.render.com/web/srv-cugdnl0gph6c73d1a7g0/deploys/dep-d1qrg1je5dus73dugaf0
+     *  versionado docker:
+     *push lionelstaricoff/veciapp:v2 : creando usuario ok
+     */
 }
